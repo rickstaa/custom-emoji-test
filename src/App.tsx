9ff3fb7a8f41d3ba0933d64b3d13ff803ae7297b
Rick Staa <rick.staa@outlook.com>
2022-08-26 21:23:16 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+import "./App.css";
+import logo from "./logo.svg";
+// import octoCat from "./octocat.svg";
+
+const custom = [
+  {
+    id: "github",
+    name: "GitHub",
+    emojis: [
+      {
+        id: "octocat",
+        name: "Octocat",
+        keywords: ["github"],
+        skins: [
+          {
+            // src: "./octocat.svg",
+            // src: { octoCat },
+            src: "https://iconape.com/wp-content/png_logo_vector/github-octocat-logo.png",
+          },
+        ],
+        custom: true,
+      },
+    ],
+  },
+];
+
+const customCategoryIcons = {
+  github: {
+    // src: "./octocat.svg",
+    // src: { octoCat },
+    src: "https://iconape.com/wp-content/png_logo_vector/github-octocat-logo.png",
+  },
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Picker
+          data={data}
+          custom={custom}
+          categoryIcons={customCategoryIcons}
+          onEmojiSelect={console.log}
+        />
       </header>
     </div>
   );
